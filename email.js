8bb2ed71484cc6fa18f843903961548fa98e5222
AnonymousUserInthisGithub <105998865+AnonymousUserInthisGithub@users.noreply.github.com>
2024-09-13 21:30:27 -0500

    email.js

--- email.js
+++ email.js
@@ -1,1 +1,14 @@
-test
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        emailjs.sendForm('service_4le8px3', 'template_9m1sk1p', this)
+            .then(function() {
+                document.getElementById('form-message').innerHTML = "Message sent successfully!";
+            }, function(error) {
+                document.getElementById('form-message').innerHTML = "Failed to send the message. Error: " + error;
+            });
+
+        this.reset();  // Reset the form after submission
+    });
+});
